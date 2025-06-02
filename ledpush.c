02a9f4763dd0d21d79d871e630f4931ee451ed47
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-06-02 06:25:37 +0530

    ledpushbutton

--- ledpush.c
+++ ledpush.c
@@ -1,2 +1,15 @@
 #define lED 12 ;
 
+voidsetup(){
+    pinMode(lED , OUTPUT);
+    pinMode(13, INPUT);
+}
+
+void loop(){
+    if (digitalRead(13)==HIGH){
+        digitalWrite(lED, HIGH);
+    }else{
+        digitalWrite(lED, LOW);
+    }
+    
+}
